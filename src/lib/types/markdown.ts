d3d62dbfe21a934f3d5fd9306bbed843d383a33a
dcog989 <sirdavidlaws@gmail.com>
2026-06-15 16:49:20 +0100

    dead code

--- src/lib/types/markdown.ts
+++ src/lib/types/markdown.ts
@@ -1,13 +1,5 @@
 export type MarkdownFlavor = 'commonmark' | 'gfm';
 
-export interface FormatterOptions {
-  flavor?: MarkdownFlavor;
-  list_indent?: number;
-  bullet_char?: '-' | '*' | '+';
-  code_block_fence?: '```' | '~~~';
-  table_alignment?: boolean;
-}
-
 export interface HeadingEntry {
   level: number;
   text: string;
